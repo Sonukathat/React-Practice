6492f88ca9feaf773e4a60a43a6533355c9164f8
sonukathat <skkhankathat93@gmail.com>
2025-04-07 10:51:35 +0530

    UI update

--- Login/src/Loginform.jsx
+++ Login/src/Loginform.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react"
+
 export function Loginform(){
+   
+    const[username,setUsername] = useState("");
+    const[password,setPassword] = useState("");
+
+    const handleUsername = (e) => {
+      setUsername(e.target.value);
+    };
+
+    const handlePassword = (e) => {
+      setPassword(e.target.value);
+    };
+    
     return(
-        <div className="shadow shadow-red-700"> 
+        <div className="flex flex-col items-center mt-20"> 
           <div>
             <h1 className="text-2xl font-bold">Login Form</h1>
           </div>
-          <div className="">
+          <div className="flex flex-col mt-5">
             <label htmlFor="username" className="">Username</label>
-            <input type="text" id="username" className=""/>
-            <label htmlFor="password" className="mt-3 font-bold">Password</label>
-            <input type="password" id="password" className="border w-70 rounded"/>
-            <button type="submit" className="bg-blue-600 p-2 rounded w-35 mt-4">Submit</button>
+            <input onChange={handleUsername} type="text" id="username" className="border rounded w-70"/>
+            <label htmlFor="password" className="">Password</label>
+            <input onChange={handlePassword} type="password" id="password" className="border rounded"/>
+            <button type="submit" className="bg-blue-600 w-25 h-8 mt-5 ml-22 rounded">Submit</button>
           </div>
         </div>
     )
